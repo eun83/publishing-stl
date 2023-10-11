82b69221d72ec2f05e913a0204fcf3d8f7246d07
최은 <neko83@technoblood.com>
2023-10-11 14:44:06 +0900

    메뉴 수정

--- html/cubersc/templete/web/js/common_new.js
+++ html/cubersc/templete/web/js/common_new.js
@@ -1,27 +1,15 @@
 $(document).ready(function () {
     
-    $('.nav > .gnb_layout > li').click(function(){
-        let $me = $(this);
-
-        if($me.is($me.parent().find('.current'))){
-            // 이전 선택한 대상과 같은 경우 닫기
-            $me.removeClass('current');
-            $me.closest('.gnb_layout').removeClass('open');
-        } else {
-            // 이전 선택한 대상과 다른 경우 새로 열기
-            $me.siblings().removeClass('current');
-            $me.addClass('current');
-            $me.closest('.gnb_layout').addClass('open');
-        }
+    // web
+    $('.nav > .gnb_layout').on('mouseover click', function() {
+        $('.nav > .gnb_layout').addClass('open');
 
         return false;
     });
-
     $('.nav').mouseleave(function(){
-        $('.nav > .gnb_layout > li').removeClass('current');
         $('.nav > .gnb_layout').removeClass('open');
     });
-
+    
     $('.btn_all_menu_open').click(function(){
         $('.all_menu').addClass('active');
         $('body').css("overflow","hidden");
@@ -32,6 +20,7 @@ $(document).ready(function () {
         $('body').css("overflow","");
     });
 
+    // search
     $('.btn_top_search_open').click(function(){
         $('.search_wrap').addClass('active');
     });
@@ -44,6 +33,7 @@ $(document).ready(function () {
         $('.search_wrap').hide().removeClass('active').show();
     });
     
+    // mobile
     $('.all_menu > .gnb_layout > li').click(function(){
         let $me = $(this);
         if($me.is($me.parent().find('.current'))){
